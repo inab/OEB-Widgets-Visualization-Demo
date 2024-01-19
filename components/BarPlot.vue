e9ca772c2e7e4369b6ad40a2d6c9c24c5205dc2a
Andrea <andrea.morales@bsc.es>
2024-01-19 17:17:25 +0100

    Feature: added quartiles table, changed buttons layout

--- components/BarPlot.vue
+++ components/BarPlot.vue
@@ -1,24 +1,37 @@
 <template>
   <div>
-    <h4>Bar Plot</h4>
 
-    <!-- <p>Dataset Id: {{ datasetId }}</p>
-    <p>Last update: {{ formattedDate }}</p> -->
-    <!-- Button Dowloand -->
-    <div class="button-container"  ref="buttonContainerRef">
-      <b-button>Sort & Classify Results</b-button>
-      <b-dropdown right text="Download" class="download-dropdown">
-        <b-dropdown-header id="dropdown-header-label">
-          Select a format
-        </b-dropdown-header>
-        <b-dropdown-item @click="downloadChart('png')">PNG</b-dropdown-item>
-        <b-dropdown-item @click="downloadChart('svg')">SVG</b-dropdown-item>
-        <b-dropdown-divider></b-dropdown-divider>
-        <b-dropdown-item @click="downloadChart('pdf')">PDF</b-dropdown-item>
-      </b-dropdown>
+    <div class="butns">
+      <!-- Buttons -->
+
+      <b-button-group class="ml-auto">
+        <!-- Button Sort -->
+        <b-button variant="outline-secondary" v-if="sortOrder === 'raw'" @click="toggleSortOrder">
+          Sort & Classify Data
+        </b-button>
+        <b-button variant="outline-secondary" v-else @click="toggleSortOrder">
+          Return To Raw Results
+        </b-button>
+        <!-- Button Optimal -->
+        <b-button variant="outline-secondary">Optimal View</b-button>
+        <!-- Button Downland -->
+        <b-dropdown variant="outline-secondary" right text="Download">
+          <b-dropdown-header id="dropdown-header-label">
+            Select a format
+          </b-dropdown-header>
+          <b-dropdown-item @click="downloadChart('png')">PNG</b-dropdown-item>
+          <b-dropdown-item @click="downloadChart('svg')">SVG</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-item @click="downloadChart('pdf')">PDF</b-dropdown-item>
+        </b-dropdown>
+      </b-button-group>
+
     </div>
+
+    <!-- Chart -->
     <div id="barPlot"></div>
     <br>
+    <!-- ID AND DATE TABLE -->
     <b-table-simple bordered small caption-top responsive>
       <b-tbody>
         <b-tr>
@@ -29,21 +42,57 @@
         </b-tr>
       </b-tbody>
     </b-table-simple>
+    <br>
+    <!-- Quartile Table -->
+    <b-container v-if="sortOrder === 'sorted'">
+      <b-row>
+        <b-col>
+          <b-card title="Quartile Data" class="mt-3">
+            <b-table :items="quartileDataArray" bordered small caption-top responsive>
+              <b-thead>
+                <b-tr>
+                  <b-th>Tool</b-th>
+                  <b-th>Quartile</b-th>
+                </b-tr>
+              </b-thead>
+              <b-tbody>
+                <b-tr v-for="(quartile, tool) in quartileDataArray" :key="tool">
+                  <b-td>{{ tool }}</b-td>
+                  <b-td>{{ quartile }}</b-td>
+                </b-tr>
+              </b-tbody>
+            </b-table>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref, nextTick  } from 'vue';
+import { onMounted, ref } from 'vue';
+import { computed } from 'vue';
+import { sortBy } from 'lodash';
 const { jsPDF } = require('jspdf');
 const html2canvas = require('html2canvas');
 
 
 
 const dataset = ref(null);
+const originalData = ref(null);
 const datasetId = ref(null);
 const datasetDate = ref(null);
 const formattedDate = ref(null);
-const buttonContainerRef = ref(null);
+const sortOrder = ref('raw');
+const quartileData = ref({});
+const quartileDataArray = computed(() => {
+  // Convert quartileData object into an array of objects
+  const array = Object.entries(quartileData.value).map(([tool, quartile]) => ({ tool, quartile }));
+  // Sort the array alphabetically by the 'tool' property
+  return array.sort((a, b) => a.tool.localeCompare(b.tool));
+});
+
 
 onMounted(async () => {
   const Plotly = require('plotly.js-dist');
@@ -53,11 +102,14 @@ onMounted(async () => {
   datasetDate.value = dataset.value.dates.modification;
   formattedDate.value = formatDateString(datasetDate.value); // Format
   const data = dataset.value.datalink.inline_data;
+  // Save original data for future use
+  originalData.value = data;
 
   const x = data.challenge_participants.map(entry => entry.tool_id);
   const y = data.challenge_participants.map(() => 0);
 
   const colors = Array(x.length).fill('#0b579f'); // Initial colors
+
 
   const initialTrace = {
     x,
@@ -97,15 +149,6 @@ onMounted(async () => {
   // Create the bar chart with the initial trace and layout
   Plotly.newPlot('barPlot', [initialTrace], layout, config);
 
-  // Use $nextTick to ensure DOM elements are fully rendered
-  await nextTick();
-
-  // Access the width of the button container and set the margin dynamically
-  const chartWidth = document.getElementById('barPlot').offsetWidth;
-  const marginRight = chartWidth * 0.1; // Adjust the percentage as needed
-  buttonContainerRef.value.style.marginRight = `${marginRight}px`;
-
-
   const myPlot = document.getElementById('barPlot');
 
 
@@ -144,6 +187,106 @@ onMounted(async () => {
 
 
 })
+function toggleSortOrder() {
+  try {
+    if (sortOrder.value === 'raw') {
+      // Sort logic (descending order)
+      const sortedData = sortBy(originalData.value.challenge_participants, entry => entry.metric_value).reverse();
+      updateChart(sortedData);
+      // Calculate quartiles and update the table data
+      quartileData.value = calculateQuartiles(sortedData);
+
+    } else {
+
+      // Return to raw data
+      updateChart(originalData.value.challenge_participants);
+      quartileData.value = {}
+
+    }
+
+    // Toggle sortOrder
+    sortOrder.value = sortOrder.value === 'raw' ? 'sorted' : 'raw';
+  } catch (error) {
+    console.error('Error in toggleSortOrder:', error);
+  }
+}
+
+function updateChart(data) {
+  const Plotly = require('plotly.js-dist');
+  const x = data.map(entry => entry.tool_id);
+  const y = data.map(entry => entry.metric_value);
+
+  const update = {
+    x: [x],
+    y: [y],
+  };
+
+  Plotly.update('barPlot', update);
+}
+
+
+// Function to calculate medians in odd or even arrays.
+function calculateMedians(inputArray) {
+  const sortedArray = [...inputArray].sort((a, b) => a - b);
+
+  // Median number
+  const middleIndex = Math.floor(sortedArray.length / 2);
+
+  if (inputArray.length % 2 === 0) {
+    // Even length
+    const middleValues = [sortedArray[middleIndex - 1], sortedArray[middleIndex]];
+    return (middleValues[0] + middleValues[1]) / 2;
+  } else {
+    // Odd length
+    return sortedArray[middleIndex];
+  }
+}
+const quartileGroupColors = ['#00ff00', '#0000ff', '#800080', '#ffa500'];
+function calculateQuartiles(data) {
+  const sortedValues = data.map(entry => entry.metric_value).sort((a, b) => a - b);
+  const middleIndex = Math.floor(data.length / 2);
+
+  let q1, q2, q3;
+  // Calculate Q2
+  if (sortedValues.length % 2 === 0) {
+    // Even length
+    q2 = (sortedValues[middleIndex - 1] + sortedValues[middleIndex]) / 2;
+  } else {
+    // Odd length
+    q2 = sortedValues[middleIndex];
+  }
+
+  const lowerArray = sortedValues.filter(value => value < q2);
+  const upperArray = sortedValues.filter(value => value > q2);
+
+  // Calculate median for lowerArray and upperArray
+  q1 = calculateMedians(lowerArray);
+  q3 = calculateMedians(upperArray);
+
+  // Create an object to store metric positions
+  const metricPositions = {};
+
+  // Assign positions to metrics based on quartiles
+  data.forEach(entry => {
+    const metricValue = entry.metric_value;
+
+    if (metricValue <= q1) {
+      metricPositions[entry.tool_id] = 1;
+    } else if (metricValue > q1 && metricValue <= q2) {
+      metricPositions[entry.tool_id] = 2;
+    } else if (metricValue > q2 && metricValue < q3) {
+      metricPositions[entry.tool_id] = 3;
+    } else if (metricValue >= q3) {
+      metricPositions[entry.tool_id] = 4;
+    }
+  });
+  console.info(metricPositions)
+
+  return metricPositions;
+}
+
+
+
 
 function downloadChart(format) {
   const Plotly = require('plotly.js-dist');
@@ -205,32 +348,18 @@ function formatDateString(dateString) {
     year: "numeric",
     month: "long",
     day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    timeZone: "UTC", // Adjust the time zone as needed
   });
 }
 
 </script>
 <style scoped>
-.button-container {
-  display: flex;
-  justify-content: right;
-}
-
-.button-container b-button {
-  margin-right: 10px;
-  /* Adjust the margin as needed */
-}
-
-.download-dropdown {
-  margin-left: 10px;
-  /* Adjust the margin as needed */
-}
-
 b-th,
 b-td {
   border-radius: 10px;
+}
+
+.butns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
