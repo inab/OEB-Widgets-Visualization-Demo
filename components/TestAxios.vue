f6a988b8907799793a3fa1fdff26c7a92cdce44a
avalle <ani.valle@bsc.es>
2024-05-09 09:35:25 +0200

    Add changes to TestAxios.vue and delete TestStore.vue

--- components/TestAxios.vue
+++ components/TestAxios.vue
@@ -1,26 +1,48 @@
 <template>
-    <div>
-      <h1>Usuarios</h1>
-      <ul>
-        <li v-for="user in users" :key="user.id">{{ user.title }}</li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    computed: {
-      users() {
-        // Obtener los usuarios del store
-        return this.$store.state.datasets;
-      },
-    },
-    async created() {
-      // Disparar la acción para obtener los datasets si aún no se han obtenido
-      if (this.$store.state.datasets.length === 0) {
-        await this.$store.dispatch('fetchDatasets');
-      }
-    },
-  };
-  </script>
-  
+  <div>
+    <h1>Métricas</h1>
+    
+    <ul>
+      <li v-for="metric in metrics" :key="metric._id">
+        {{ metric.title }}
+        <span>{{ metric._id }}</span>
+        <span>{{ metric._metadata }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script> 
+export default {
+  data() {
+    return {
+      metrics: [],
+    };
+  },
+  async mounted() {
+    let metric_x = 'avg Schlicker'
+    let metric_y = 'NR_ORTHOLOGS'
+
+    // Disparar la acción para obtener las métricas si aún no se han obtenido
+    if (this.metrics.length === 0) {
+      await this.$store.dispatch('fetchMetrics');
+      // Una vez que se han obtenido las métricas, asignarlas a la propiedad metrics
+      this.metrics = this.$store.state.metrics;
+
+      let metrics_names = {'metricX': '', 'metricY': ''};
+      this.metrics.forEach(function(metric) {
+        if(metric._metadata && "level_2:metric_id" in metric._metadata){
+          if(metric._metadata["level_2:metric_id"] === metric_x){
+            metrics_names['metricX'] = metric.title
+          }
+          if(metric._metadata["level_2:metric_id"] === metric_y){
+            metrics_names['metricY'] = metric.title
+          }
+        }
+      });
+      console.log(metrics_names)
+      
+    }
+  }
+};
+</script>
